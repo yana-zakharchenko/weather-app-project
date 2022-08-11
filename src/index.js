fc4ac5a3861de972c3b72c2ffdd9e813d5443488
yana-zakharchenko <yaninaayana@gmail.com>
2022-08-11 13:47:45 +0300

    added api to forecast

--- src/index.js
+++ src/index.js
@@ -22,7 +22,8 @@ if (minutes < 10) {
 let currentTime = document.querySelector("#current-date-time");
 currentTime.innerHTML = `${day} ${hours}:${minutes}`;
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily)
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
@@ -39,6 +40,13 @@ function displayForecast() {
 
 forecastHTML = forecastHTML + `</div>`
 forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates){
+  let apiKey = "ce48950d7371479b3506a93b3340eea5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl)
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showTemp(response) {
@@ -58,6 +66,8 @@ function showTemp(response) {
   let descriptionElement = document.querySelector("#weather-description");
   descriptionElement.innerHTML = `${description}`
   console.log(response.data.name);
+
+  getForecast(response.data.coord)
 }
 
 function search(city) {
@@ -115,4 +125,4 @@ let currentButton = document.querySelector("#button-current");
 currentButton.addEventListener("click", getCurrentPosition);
 
 search("Kyiv");
-displayForecast();
+
